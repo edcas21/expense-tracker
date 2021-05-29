0fdb82a0f430431ef3879f115535b29343ab9af3
Edgar <cardenasedgar123@gmail.com>
2021-05-28 20:55:54 -0400

    Created Details component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import classes from "*.module.css";
+
+function Main() {
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+    </Card>
+  );
+}
+
+export default Main;
