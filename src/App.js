0362c4386da86c91e14bdf12ab9d629ae98381bb
Edgar <cardenasedgar123@gmail.com>
2021-05-28 21:35:19 -0400

    Created Main and Form component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
 
+import Main from "./components/Main/Main";
 import Details from "./components/Details/Details";
 import useStyles from "./styles";
 
@@ -10,7 +11,7 @@ const App = () => {
   return (
     <div>
       <Grid
-        className={classes.Grid}
+        className={classes.grid}
         container
         spacing={0}
         alignItems="center"
@@ -20,11 +21,11 @@ const App = () => {
         <Grid item xs={12} sm={4}>
           <Details title="Income" />
         </Grid>
-        <Grid item xs={12} sm={4}>
-          Main
+        <Grid item xs={12} sm={3}>
+          <Main />
         </Grid>
         <Grid item xs={12} sm={4}>
-          <Details title="Expense"/>
+          <Details title="Expense" />
         </Grid>
       </Grid>
     </div>
