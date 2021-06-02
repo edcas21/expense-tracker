dbfffdca62b6782d861b4ba1315d22b73b8447e4
Edgar <cardenasedgar123@gmail.com>
2021-06-02 12:58:25 -0400

    Created index.js and App.js files. Added styling to index file.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Provider from './context/context';
-import App from "./App";
-import "./index.css";
+import App from './App';
+import './index.css';
 
-ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById('root'));
