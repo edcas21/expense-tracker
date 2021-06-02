0fcc4cc4832ecfc9f33940fabf0824482a975c1c
Edgar <cardenasedgar123@gmail.com>
2021-06-02 13:25:48 -0400

    Created Details component and added styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import { Details } from "./components";
 
 const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
-}
+  return (
+    <div>
+      <Grid
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          Main
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default App
+export default App;
